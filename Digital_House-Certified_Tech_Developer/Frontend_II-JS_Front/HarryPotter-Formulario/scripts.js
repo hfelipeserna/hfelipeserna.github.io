557f3e4bcc5519ac3b6b4d0d81c0a952e4876b92
Hernán Felipe Serna Gómez <hfserna@gmail.com>
2021-06-27 07:18:07 -0500

    [NEW] Se agrega proyecto HarryPotter Formulario

---
+++ Digital_House-Certified_Tech_Developer/Frontend_II-JS_Front/HarryPotter-Formulario/scripts.js
@@ -0,0 +1,75 @@
+window.onload = () => {
+  const formInscripcion = document.forms.inscripcion;
+
+  const nombre = formInscripcion.nombre;
+  const contrasenia = formInscripcion.pass;
+  const telefono = formInscripcion.tel;
+  const hobbies = formInscripcion.hobbies;
+  const nacionalidad = formInscripcion.nacionalidad;
+
+  console.log(hobbies)
+  console.log(nacionalidad.value)
+
+  const boton = formInscripcion.querySelector('button');
+
+  const datos = {
+    nombreCompleto: null,
+    contrasenia: null,
+    telefono: null,
+    hobbies: [],
+    nacionalidad: null
+  }
+
+  nombre.addEventListener('keyup', () => {
+    datos.nombreCompleto = nombre.value.toLowerCase().trim();
+  })
+
+  contrasenia.addEventListener('keyup', () => {
+    datos.contrasenia = contrasenia.value.trim();
+  })
+
+  telefono.addEventListener('keyup', () => {
+    const number = parseInt(telefono.value);
+
+    if (isNaN(number)) {
+      boton.disabled = true;
+      datos.telefono = null;
+    } else {
+      boton.disabled = false;
+      datos.telefono = telefono.value;
+    }
+
+  })
+
+  formInscripcion.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    // const ejemplo = [{ prop: 'checked' }, { prop: 'unchecked' }, { prop: 'checked' }]
+
+    // const hobbiesSelected = ejemplo.filter(ej => {
+    //   return ej.prop == 'checked'
+    // })
+
+    // console.log(hobbiesSelected)
+
+    hobbies.forEach( hobbie => {
+      if(hobbie.checked) {
+        datos.hobbies.push(hobbie.value);
+      }
+    }) 
+    console.log(datos);
+  })
+
+}
+
+// class NombreClase {
+//   constructor(nombre) {
+//     this.propiedad = npmbre
+//   }
+
+// }
+// new NombreClase('fernando');
+
+// nombre completo
+// contraseña
+// telefono
