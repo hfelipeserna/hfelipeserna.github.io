1c7598967e1dd9ef7c857c25d788e1b1cb22f2bc
Hernán Felipe Serna Gómez <hfserna@gmail.com>
2021-08-22 23:00:04 -0500

    [FIX] Fix bug
    - Se corrige bug el cual generaba error al traer un producto nulo sobre el cual se debía actualizar las existencias.
    - Se implementó código para las clases correspondientes a la carpeta "service"

--- Digital_House-Certified_Tech_Developer/Backend_I/Facade_patron/Servicio_de_compra/src/service/implementation/PagoService.java
+++ Digital_House-Certified_Tech_Developer/Backend_I/Facade_patron/Servicio_de_compra/src/service/implementation/PagoService.java
@@ -1,4 +1,14 @@
 package service.implementation;
 
+import model.Tarjeta;
+
 public class PagoService {
+
+    public Boolean procesarPago(Tarjeta tarjeta, Integer montoACobrar){
+        Boolean pagoRealizado = Boolean.FALSE;
+        if(tarjeta != null && tarjeta.getNumero() != null & tarjeta.getCVV() != null){
+            System.out.println("Procesando el pago por $" + String.valueOf(montoACobrar));
+        }
+        return pagoRealizado;
+    }
 }
